7386de922e42d9e3ece3961c820d695a4d9ea88b
ZoeAcero <zar.amazona@gmail.com>
2024-10-22 18:04:32 +0200

    a

---
+++ src/main/java/Elemento_Coleccionable/ElementoColeccionable.java
@@ -0,0 +1,11 @@
+package Elemento_Coleccionable;
+
+public class ElementoColeccionable {
+    private String pais;
+    private String autoridadGobernante;
+    private int annus;
+    private double valor;
+    private String unidadMonetaria;
+    private int rareza;
+    private double precio;
+}
