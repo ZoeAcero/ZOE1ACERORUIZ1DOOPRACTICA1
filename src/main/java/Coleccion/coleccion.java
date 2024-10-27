1d7d3f028125d8718d3b76b3dba1318d368f62b6
ZoeAcero <zar.amazona@gmail.com>
2024-10-27 12:41:50 +0100

    a

--- src/main/java/Coleccion/coleccion.java
+++ src/main/java/Coleccion/coleccion.java
@@ -1,7 +1,8 @@
 package Coleccion;
 import java.util.ArrayList;
 import java.util.List;
+import Elemento_Coleccionable.ElementoColeccionable;
 
 public class coleccion {
-    
+    private List<ElementoColeccionable> elementos;
 }
