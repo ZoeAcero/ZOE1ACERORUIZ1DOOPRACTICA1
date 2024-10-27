551e2b7937ed607d38d81b531badac1d948fd848
ZoeAcero <zar.amazona@gmail.com>
2024-10-27 12:38:56 +0100

    a

--- src/main/java/Sello/sello.java
+++ src/main/java/Sello/sello.java
@@ -8,4 +8,14 @@ public class sello extends ElementoColeccionable{
     private double anchura;
     private String imagen;
     private EstadoConservacionSello estadoConservacion;
+
+    public sello(String pais, String autoridadGobernante, int annus, double valor, String unidadMonetaria, int rareza, double precio, double altura, double anchura, String imagen, EstadoConservacionSello estadoConservacion) {
+        super(pais, autoridadGobernante, annus, valor, unidadMonetaria, rareza, precio);
+        this.altura = altura;
+        this.anchura = anchura;
+        this.imagen = imagen;
+        this.estadoConservacion = estadoConservacion;
+    }
+
+    // Getters y Setters
 }
