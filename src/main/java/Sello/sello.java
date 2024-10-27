d8306789fb4a511c775d5e7c5d8e4a7ebe3038de
ZoeAcero <zar.amazona@gmail.com>
2024-10-27 12:38:23 +0100

    a

--- src/main/java/Sello/sello.java
+++ src/main/java/Sello/sello.java
@@ -1,7 +1,11 @@
 package Sello;
 import Elemento_Coleccionable.ElementoColeccionable;
+import Enums.EstadoConservacionSello;
 
 public class sello extends ElementoColeccionable{
 
-    
+    private double altura;
+    private double anchura;
+    private String imagen;
+    private EstadoConservacionSello estadoConservacion;
 }
