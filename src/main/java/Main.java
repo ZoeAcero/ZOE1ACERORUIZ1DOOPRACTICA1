152bced8c8117b8a7aafad68d9b17952e48738a6
ZoeAcero <zar.amazona@gmail.com>
2024-10-27 12:48:55 +0100

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,5 +11,16 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int opcion;
 
-        
+
+        do {
+            System.out.println("Menú:");
+            System.out.println("1-Añadir una nueva moneda");
+            System.out.println("2-Añadir un nuevo sello");
+            System.out.println("3-Mostrar todas las monedas");
+            System.out.println("4-Mostrar todos los sellos");
+            System.out.println("5-Mostrar el precio total de la colección");
+            System.out.println("6-Mostrar la rareza media de la colección");
+            System.out.println("0-Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
 }
