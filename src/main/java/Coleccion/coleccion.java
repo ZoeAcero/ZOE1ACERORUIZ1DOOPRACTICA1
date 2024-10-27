49201aa59f1c0fbd2f61666397b9fe1cd6af70ff
ZoeAcero <zar.amazona@gmail.com>
2024-10-27 12:43:00 +0100

    a

--- src/main/java/Coleccion/coleccion.java
+++ src/main/java/Coleccion/coleccion.java
@@ -5,4 +5,12 @@ import Elemento_Coleccionable.ElementoColeccionable;
 
 public class coleccion {
     private List<ElementoColeccionable> elementos;
+
+    public coleccion() {
+        this.elementos = new ArrayList<>();
+    }
+
+    public void anadirMoneda(Moneda moneda) {
+        elementos.add(moneda);
+    }
 }
