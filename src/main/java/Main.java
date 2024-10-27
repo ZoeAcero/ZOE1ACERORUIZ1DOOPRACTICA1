71f15d3b738aa029fcab5f8705de029285f8d4e1
ZoeAcero <zar.amazona@gmail.com>
2024-10-27 12:55:52 +0100

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import Coleccion.coleccion;
 import Moneda.moneda;
 import Sello.sello;
-import Elemento_Coleccionable.ElementoColeccionable;
 import Enums.*;
 
 public class Main {
