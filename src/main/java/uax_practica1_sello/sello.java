3b115a53947b14d2c90a90e64c80a59ed88d0cdd
ZoeAcero <zar.amazona@gmail.com>
2024-10-29 17:21:26 +0100

    a

--- src/main/java/uax_practica1_sello/sello.java
+++ src/main/java/uax_practica1_sello/sello.java
@@ -1,15 +1,19 @@
 package uax_practica1_sello;
+
 import uax_practica1_elemento_coleccionable.ElementoColeccionable;
 import uax_practica1_enums.EstadoConservacionSello;
 
-public class sello extends ElementoColeccionable{
+public class sello extends ElementoColeccionable {
 
+    // Atributos específicos de la clase Sello
     private double altura;
     private double anchura;
     private String imagen;
     private EstadoConservacionSello estadoConservacion;
 
+    // Constructor de la clase Sello
     public sello(String pais, String autoridadGobernante, int annus, double valor, String unidadMonetaria, int rareza, double precio, double altura, double anchura, String imagen, EstadoConservacionSello estadoConservacion) {
+        // Llamada al constructor de la clase padre ElementoColeccionable
         super(pais, autoridadGobernante, annus, valor, unidadMonetaria, rareza, precio);
         this.altura = altura;
         this.anchura = anchura;
@@ -19,38 +23,47 @@ public class sello extends ElementoColeccionable{
 
     // Getters y Setters
 
+    // Obtener la altura del sello
     public double getAltura() {
         return altura;
     }
 
+    // Establecer la altura del sello
     public void setAltura(double altura) {
         this.altura = altura;
     }
 
+    // Obtener la anchura del sello
     public double getAnchura() {
         return anchura;
     }
 
+    // Establecer la anchura del sello
     public void setAnchura(double anchura) {
         this.anchura = anchura;
     }
 
+    // Obtener la imagen del sello
     public String getImagen() {
         return imagen;
     }
 
+    // Establecer la imagen del sello
     public void setImagen(String imagen) {
         this.imagen = imagen;
     }
 
+    // Obtener el estado de conservación del sello
     public EstadoConservacionSello getEstadoConservacion() {
         return estadoConservacion;
     }
 
+    // Establecer el estado de conservación del sello
     public void setEstadoConservacion(EstadoConservacionSello estadoConservacion) {
         this.estadoConservacion = estadoConservacion;
     }
 
+    // Método toString para representar el objeto Sello como una cadena
     @Override
     public String toString() {
         return "Sello{" +
